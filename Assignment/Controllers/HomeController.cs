8f6cec316365bc44e4dc911e1dc688db36ec21a8
ZERO <yuxuan674@gmail.com>
2026-07-09 21:07:24 +0800

    Add project files.

---
+++ Assignment/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment.Controllers
+{
+    public class HomeController(DB db) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Assignment/Models/DB.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Assignment.Models;
+
+#nullable disable warnings
+public class DB(DbContextOptions options) : DbContext(options)
+{
+
+}
